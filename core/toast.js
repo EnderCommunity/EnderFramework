d9eed225416e1e441f2e41ef9f489c18b8dd1297
EnderAdel <57331033+EnderAdel@users.noreply.github.com>
2020-12-24 10:39:00 +0200

    Added a manifest checker

--- core/toast.js
+++ core/toast.js
@@ -1,14 +1,15 @@
-const nodeNotifier = require('node-notifier'), notify = (title, message) => {
-    nodeNotifier.notify({
-        title: title,
-        message: message,
-        icon: "0",
-        id: undefined,
-        appID: "EnderServices"
-    }, function (err, response) {
-        throw Error(`Title: ${title}\nMessage: ${message}`);
-    });
-};
+const nodeNotifier = require('node-notifier'),
+    notify = (title, message) => {
+        nodeNotifier.notify({
+            title: title,
+            message: message,
+            icon: "0",
+            id: undefined,
+            appID: "EnderServices"
+        }, function(err, response) {
+            //throw Error(`Title: ${title}\nMessage: ${message}`);
+        });
+    };
 module.exports = {
     notify: (title, message) => {
         notify(title, message);
