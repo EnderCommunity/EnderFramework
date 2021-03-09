9cf235752170f2a8688ad0abeeb7a948ebc760e1
adel-sbeh <72353925+adel-sbeh@users.noreply.github.com>
2021-03-10 01:23:23 +0200

    Added the `platform` attribute

--- core/framework/scripts/content/theme.js
+++ core/framework/scripts/content/theme.js
@@ -1,13 +1,17 @@
-module.exports = function () {
-    var isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches, isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches, isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches, hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+module.exports = function() {
+    var isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches,
+        isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches,
+        isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches,
+        hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
     if (isDarkMode)
         document.documentElement.setAttribute('prefers-color-scheme', 'dark');
     else
         document.documentElement.setAttribute('prefers-color-scheme', 'light');
-    ipcRenderer.on("enderframework--theme-tolight", function () {
+    ipcRenderer.on("enderframework--theme-tolight", function() {
         document.documentElement.setAttribute("prefers-color-scheme", "light");
     });
-    ipcRenderer.on("enderframework--theme-todark", function () {
+    ipcRenderer.on("enderframework--theme-todark", function() {
         document.documentElement.setAttribute("prefers-color-scheme", "dark");
     });
+    document.documentElement.setAttribute("platform", require("./../../../platform").getNameCode());
 };
